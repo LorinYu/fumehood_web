9caab639c1579e71325e8a7933ee9ff2461878c3
LorinYu <1181401581@qq.com>
2017-07-21 16:29:57 +0800

    Delete pic.php

--- pic.php
+++
@@ -1,9 +0,0 @@
-<?php
-        header("Content-Type:text/html; charset=utf-8");
-	      $picname='pic.png';
-
-	      if(file_exists($picname)){unlink($picname);}
-
-        $pic = $_FILES['pic'];
-        move_uploaded_file($pic['tmp_name'],'./Public/pic/pic.png');
-?>
